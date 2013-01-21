da63cf3db96df0641cf236a26a6887c0200b7d79
Dalton Dick <daltondick2010@gmail.com>
2013-01-21 01:57:56 -0600

    Added authorization for edit and update on users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -46,5 +46,24 @@ describe "AuthenticationPages" do
                 it { should have_link('Sign in') }
             end
         end
+
+        describe "authorization" do
+
+            describe "for non signed-in users" do
+                let(:user) { FactoryGirl.create(:user) }
+
+                describe "in the Users controller" do
+                    describe "visiting the edit page" do
+                        before { visit edit_user_path(:user) }
+                        it { should have_selector('title') }
+                    end
+
+                    describe "submitting to the update section" do
+                        before { put user_path(user) }
+                        specify { response.should redirect_to(signin_path) }
+                    end
+                end
+            end
+        end
     end
 end
